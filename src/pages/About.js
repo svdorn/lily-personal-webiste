fb8fe09a75e8900b31d2ba19c0a292fbd12684cd
Stephen Dorn <stevedorn9@gmail.com>
2019-03-23 17:51:08 -0700

    swirl modularized

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Swirl from "../components/Swirl";
+
+import "./Pages.css";
 
 class About extends React.Component {
     render() {
-        return <div>About page</div>;
+        return (
+            <div className="container">
+                <div className="first-frame-container about-first-frame">
+                    <div>About</div>
+                    <Swirl />
+                </div>
+            </div>
+        );
     }
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
 import Card from "../components/Card";
+import Swirl from "../components/Swirl";
 
-import "./Home.css";
+import "./Pages.css";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
@@ -22,8 +23,8 @@ class Home extends React.Component {
         const { activeStep } = this.state;
 
         return (
-            <div className="home-container">
-                <div className="first-frame-container">
+            <div className="container">
+                <div className="first-frame-container home-first-frame">
                     <div className="home">
                         <Bio />
                         <Images activeStep={activeStep} handleStepChange={this.handleStepChange} />
@@ -35,18 +36,6 @@ class Home extends React.Component {
         );
     }
 }
-
-const Swirl = () => (
-    <div className="swirly">
-        <svg width={2013} height={172} viewBox="0 0 2013 172">
-            <path
-                fill="#faf9f7"
-                fillrule="evenodd"
-                d="M 533.92 171.687 h 1479.53 c -89.242 -1.566 -149.858 -6.098 -181.848 -13.595 c -131.213 -30.753 -189.294 -82.86 -330.47 -91.905 c -179.91 -11.527 -348.43 69.52 -517.539 52.257 C 736.719 93.24 708.709 0.31 528.737 0.31 C 405.42 0.31 315.541 78.92 201.16 141.23 C 149.456 169.393 -0.554 171.686 -0.554 171.686 H 533.92 Z"
-            />
-        </svg>
-    </div>
-);
 
 const Cards = () => {
     return (
