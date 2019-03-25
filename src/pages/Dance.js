f8643fe36d20e4444fa49fca6064716468e709bb
Stephen Dorn <stevedorn9@gmail.com>
2019-03-25 00:55:57 -0700

    new vids

--- src/pages/Dance.js
+++ src/pages/Dance.js
@@ -38,6 +38,6 @@ const Videos = () => (
     </div>
 );
 
-const videoIds = ["CR7Mqgipnbc", "eR_SrN8xz90", "1YUkzn6SWCI", "J7yqAfRpEi0"];
+const videoIds = ["CR7Mqgipnbc", "eR_SrN8xz90", "1YUkzn6SWCI", "0no8zNM05uk", "J7yqAfRpEi0"];
 
 export default Dance;
