902fa0ed84e6d3e53879065580a9d1a6cd96e312
Stephen Dorn <stevedorn9@gmail.com>
2019-03-25 01:16:51 -0700

    fix about page to fit design

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,20 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
 import Swirl from "../components/Swirl";
-
-import { animateScroll } from "react-scroll";
 
 import "./Pages.css";
 
 class About extends React.Component {
-    handleClick = () => {
-        animateScroll.scrollTo(800);
-    };
-
     render() {
         return (
             <div className="container">
-                <div className="first-frame-container about-first-frame">
-                    <Bio />
-                    <AboutButton handleClick={this.handleClick} />
+                <div className="first-frame-container dance-first-frame">
+                    <h1 className="header">About Lily</h1>
                     <Swirl />
+                </div>
+                <div className="philosophy">
+                    <Bio />
+                    <img src="/images/Lily/Headshot_2.jpg" alt="Headshot_2" />
                 </div>
                 <Resume />
             </div>
@@ -25,19 +22,13 @@ class About extends React.Component {
     }
 }
 
-const AboutButton = ({ handleClick }) => (
-    <div className="button-container">
-        <Button onClick={handleClick} className="button" size="large">
-            View Resume
-        </Button>
-    </div>
-);
-
 const Bio = () => {
     return (
-        <div className="about-bio-container">
-            <h1>About Lily</h1>
+        <Paper elevation={3} square={false}>
             <div>
+                <b>Bio</b>
+            </div>
+            <p>
                 Lily Thongnuam is originally from Madison, Wisconsin where she started dancing at
                 age 3. She is trained in a variety of styles since a young age, including
                 contemporary, jazz, hip hop, modern, ballet, ballroom, and tap. She studied at
@@ -51,19 +42,24 @@ const Bio = () => {
                 Performance class, she performed/choreographed on the “Zumanity” Cirque du Soleil
                 stage in Las Vegas. She also had the pleasure working with acclaimed choreographers
                 Hamilton Evans, Nick Lanzisera, Ido Tadmor, and Justin Giles for the dance touring
-                ensemble. Lily has a strong passion for teaching and choreographing. During her time
-                at Chapman, she has choreographed for multiple shows, such as Spring Dance Concert,
+                ensemble.
+            </p>
+            <p>
+                Lily has a strong passion for teaching and choreographing. During her time at
+                Chapman, she has choreographed for multiple shows, such as Spring Dance Concert,
                 Concert Intime, and Chapman Dance Alliance. Pieces that she has choreographed for
                 dance studios have received many awards including choreography, technical, and
                 musicality awards. As a student, she participates in prestigious organizations like
                 National Dance Education Organization. Lily is also a certified yoga instructor and
-                is actively teaching in the fitness community. In the commercial world, she can be
-                seen as a featured dancer on CBS’s “The Bold and the Beautiful” alongside Nia Sioux.
-                She has worked on short films involving dance and has produced/directed her own
-                dance films. After graduation, Lily looks forward moving to Los Angeles to continue
-                to pursue a commercial dance career.
-            </div>
-        </div>
+                is actively teaching in the fitness community.
+            </p>
+            <p>
+                In the commercial world, she can be seen as a featured dancer on CBS’s “The Bold and
+                the Beautiful” alongside Nia Sioux. She has worked on short films involving dance
+                and has produced/directed her own dance films. After graduation, Lily looks forward
+                moving to Los Angeles to continue to pursue a commercial dance career.
+            </p>
+        </Paper>
     );
 };
 
