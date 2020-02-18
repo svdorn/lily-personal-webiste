dfe59bde73884077e4e7d1f8e2a387dd571d6dfc
Stephen Dorn <steved11@sbcglobal.net>
2020-02-17 18:02:45 -0800

    fixing http redirects

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import App from "./App";
+import HttpsRedirect from "react-https-redirect";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+    <HttpsRedirect>
+        <App />
+    </HttpsRedirect>,
+    document.getElementById("app")
+);
 registerServiceWorker();
